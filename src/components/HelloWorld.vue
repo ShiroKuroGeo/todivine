0315f02606b441ef45d29070d1cc27797d1ca918
ShiroKuroGeo <inocgeorgealfeser@gmail.com>
2024-02-16 18:49:01 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page-content">
+    <div id="content">
+      <header>
+        <div class="cover bg-light">
+          <div class="container px-3 vh-75">
+            <div class="row">
+              <div class="d-flex justify-content-center">
+                <div class="col-lg-6">
+                  <div class="mt-5">
+                    <p class="lead text-uppercase mb-1">Happy Valentine's Day</p>
+                    <h1 class="intro-title marker" data-aos="fade-left" data-aos-delay="50">
+                      To the one who fills my heart with warmth and joy, Happy Valentine's Day! Your love is the sweetest
+                      gift I could ever ask for.
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div class="wave-bg"></div>
+    </div>
+    <small>PS: Sorry for the late Valentine's Day message, Pumall. I Love You So Much!!</small>
+  </div>
+</template>
